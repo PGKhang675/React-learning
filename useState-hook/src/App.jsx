d5e121645d6015b2558095ee34aca6a2d291fe4d
PGKhang675 <phunggiakhang675@gmail.com>
2025-01-16 11:29:28 +0700

    userState() hook added

---
+++ useState-hook/src/App.jsx
@@ -0,0 +1,17 @@
+// React hook = Special functions that allow functional components to use React featrues 
+//              without writing class components
+//              (useState(), useEffect(), useContext(), userReducer(), useCallback(), ...)
+
+// useState() = A React hook that allows a creation of a stateful variable and a Setter function
+//              to update its value in the virtual DOM.
+//              [name, setName]
+
+import MyComponent from "./MyComponent";
+
+function App(){
+  return(
+    <MyComponent></MyComponent>
+  );
+}
+
+export default App
---
+++ useState-hook/src/MyComponent.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+function MyComponent(){
+
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+
+    const updateName = () => {
+        setName("Spongebob"); 
+    }
+
+    const updateAge = () => {
+        setAge(age + 1)
+    }   
+
+    return(
+        <div>
+            <p>Name: {name}</p>
+            <button onClick =  {updateName}>Set Name</button>
+
+            <p>Age: {age}</p>
+            <button onClick =  {updateAge}>Increment Age</button>
+        </div>
+    )
+}
+
+export default MyComponent
